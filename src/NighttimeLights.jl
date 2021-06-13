5df677e4270d181cbfe2aed6ca733794db93b491
Ayush Patnaik <u6012645@anu.edu.au>
2021-06-13 21:14:25 +0530

    exporting coordinate_system functions

--- src/NighttimeLights.jl
+++ src/NighttimeLights.jl
@@ -5,4 +5,6 @@ using ArchGDAL
 using GeoArrays
 export load_img, load_datacube, save_img, save_datacube
 include("data_io.jl")
+export lat_to_row, row_to_lat, lat_to_row, long_to_column, translate_coordinate_system, Coordinate, CoordinateSystem
+include("coordinate_system.jl")
 end
