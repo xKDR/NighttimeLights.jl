191438a602230bf617b32269571afd023caa5e13
Ayush Patnaik <u6012645@anu.edu.au>
2021-06-14 14:50:44 +0530

    using cubicspline to model curvature of earth for accurate estimate of area

--- src/NighttimeLights.jl
+++ src/NighttimeLights.jl
@@ -11,11 +11,13 @@ using GeometricalPredicates
 using ProgressMeter
 using SparseArrays
 using StatsBase
+using Loess
+using CubicSplines
 
 export load_img, load_datacube, save_img, save_datacube
 include("data_io.jl")
 export lat_to_row, row_to_lat, lat_to_row, long_to_column, translate_coordinate_system, Coordinate, CoordinateSystem, india
 include("coordinate_system.jl")
-export polygon_mask, load_shapefile
+export polygon_mask, load_shapefile, mask_area, LAT, EW, NS, MODEL_EW, MODEL_NS
 include("polygons.jl")
 end
