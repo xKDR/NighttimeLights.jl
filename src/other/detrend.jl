ad1ff5687403ff432c928358071cd11bdcf3c471
Ayush Patnaik <u6012645@anu.edu.au>
2021-06-17 13:20:18 +0530

    adding libs used in backend

---
+++ src/other/detrend.jl
@@ -0,0 +1,13 @@
+
+function detrend_ts(arr)
+    """
+    This function removes time trend from a time series (array)
+    """
+    x = collect(0:1/12:1/12*(length(arr)-1))
+    x = convert(Array{Float64,1},x)
+    y = convert(Array{Float64},arr)
+    data = DataFrame(X=x, Y=y)
+    ols = lm(@formula(Y ~ X), data)
+    f(x) = coef(ols)[1] + coef(ols)[2]*x
+    return (arr - f.(x))
+end
