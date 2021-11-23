7523a1e0d2d86199df9105be25032f4d2a8a6314
Ayush Patnaik <u6012645@anu.edu.au>
2021-11-23 15:48:42 +0530

    adding error handling for a special case.

--- src/other/weighted_mean.jl
+++ src/other/weighted_mean.jl
@@ -1,4 +1,7 @@
 function weighted_mean(numbers, weights)
+    if sum(weights) == 0 
+        return 0 # Such a pixel is background noise for us 
+    end
     tmp_weights = []
     tmp_number = []
     for i in 1:length(weights)
