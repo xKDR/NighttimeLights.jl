e16341074836cdd2e3e072b2d503197c203c1279
Ayush Patnaik <u6012645@anu.edu.au>
2021-11-24 13:09:43 +0530

    Created another exception handling for a specific case.

--- src/other/weighted_mean.jl
+++ src/other/weighted_mean.jl
@@ -1,13 +1,8 @@
 function weighted_mean(numbers, weights)
-    if sum(weights) == 0 
-        return 0 # Such a pixel is background noise for us 
-    end
     tmp_weights = []
     tmp_number = []
     for i in 1:length(weights)
         if isnan(numbers[i])
-            continue
-        elseif weights[i] == 0
             continue
         else
             push!(tmp_weights, weights[i])
@@ -16,9 +11,16 @@ function weighted_mean(numbers, weights)
     end
     weights = tmp_weights
     numbers = tmp_number
-    wm = 0
-    for i in 1:length(numbers)
-        wm = wm + numbers[i]*weights[i]
+    if length(weights) == 0
+        return 0
     end
-    return wm/sum(weights)
+    if sum(tmp_weights) == 0 
+        return 0 # Such a pixel is background noise for us 
+    end
+    
+    wm = 0
+    for i in 1:length(tmp_number)
+        wm = wm + tmp_number[i]*tmp_weights[i]
+    end
+    return wm/sum(tmp_weights)
 end
