30405735396dfe73a39f93dff404247c63bf2486
Ayush Patnaik <u6012645@anu.edu.au>
2021-11-24 23:03:07 +0530

    attempting to remove luxor.jl

--- src/NighttimeLights.jl
+++ src/NighttimeLights.jl
@@ -1,6 +1,6 @@
 module NighttimeLights
 
-using Dates, ArchGDAL, GeoArrays, DataFrames, Shapefile, Luxor, GeoInterface, ProgressMeter, SparseArrays, StatsBase, Statistics, SmoothingSplines, CubicSplines, GLM, RecursiveArrayTools, JLD, Distributions, HypothesisTests
+using Dates, ArchGDAL, GeoArrays, DataFrames, Shapefile, GeoInterface, ProgressMeter, SparseArrays, StatsBase, Statistics, SmoothingSplines, CubicSplines, GLM, RecursiveArrayTools, JLD, Distributions, HypothesisTests
 
 export load_img, load_datacube, save_img, save_datacube, 
 lat_to_row, row_to_lat, lat_to_row, long_to_column, column_to_long, translate_geometry, Coordinate, CoordinateSystem, image_to_coordinate,
