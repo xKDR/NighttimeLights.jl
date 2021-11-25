fe3fe1df58646ad8a7724b27f5f4f965403167b6
= <=>
2021-11-25 12:17:43 +0100

    Adding function for light per area of a dataframe.

--- src/aggregate.jl
+++ src/aggregate.jl
@@ -21,29 +21,60 @@ aggregate_timeseries(rand_datacube, rand_mask)
 ```
 """
 function aggregate_timeseries(datacube, mask)
-    datacube = convert(Array{Float32,3}, datacube)
-    datacube = sparse_cube(copy(datacube))
+    if typeof(datacube) == Array{Float16, 3}
+        @warn "Float 16 may not be sufficient to aggregate."
+    end
     datacube = apply_mask(datacube, mask)
-    lights = cross_apply(sum, datacube)
-    return lights
+    lights = []
+    for i in 1:length(datacube)
+        push!(lights, sum(ts[i]))
+    end
+    return Float32.(lights)
 end
 
 """
 Computes the time series of aggregate value of datacube for each row of a shapefile
 # Example:
 ```
-julia> rand_datacube = rand(10, 10, 10)
-julia> shapefile_df  = load_shapefile("assets/mumbai_map/mumbai_districts.shp")
-julia> aggregate_dataframe(MUMBAI_COORDINATE_SYSTEM, rand_datacube, shapefile_df, "District")
+rand_datacube = rand(10, 10, 10)
+shapefile_df  = load_shapefile("assets/mumbai_map/mumbai_districts.shp")
+aggregate_dataframe(MUMBAI_COORDINATE_SYSTEM, rand_datacube, shapefile_df, "District")
 ```
 """
 function aggregate_dataframe(geometry::CoordinateSystem, datacube, shapefile_df, attribute)
+    if typeof(datacube) == Array{Float16, 3}
+        @warn "Float 16 may not be sufficient to aggregate."
+    end
+    datacube = sparse_cube(datacube)
     df = DataFrame()
     @showprogress for i in 1:length(shapefile_df[:, 1])
         shapefile_row = shapefile_df[i, :]
         geom_polygon = polygon_mask(geometry, shapefile_row)
-        data = aggregate_timeseries(datacube,geom_polygon)
-        df[!, shapefile_df[!, attribute][i]] = data
+        df[!, shapefile_df[!, attribute][i]] = aggregate_timeseries(datacube, geom_polygon)
+        i = i + 1
+    end
+    return df
+end
+
+"""
+Computes the time series of aggregate value per area of datacube for each row of a shapefile
+# Example:
+```
+rand_datacube = rand(10, 10, 10)
+shapefile_df  = load_shapefile("assets/mumbai_map/mumbai_districts.shp")
+aggregate_per_area_dataframe(MUMBAI_COORDINATE_SYSTEM, rand_datacube, shapefile_df, "District")
+```
+"""
+function aggregate_per_area_dataframe(geometry::CoordinateSystem, datacube, shapefile_df, attribute)
+    if typeof(datacube) == Array{Float16, 3}
+        @warn "Float 16 may not be sufficient to aggregate."
+    end
+    datacube = sparse_cube(datacube)
+    df = DataFrame()
+    @showprogress for i in 1:length(shapefile_df[:, 1])
+        shapefile_row = shapefile_df[i, :]
+        geom_polygon = polygon_mask(geometry, shapefile_row)
+        df[!, shapefile_df[!, attribute][i]] = aggregate_timeseries(datacube, geom_polygon) ./mask_area(geom_polygon)
         i = i + 1
     end
     return df
