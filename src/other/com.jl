2fe523e04ced875a5f9e5a6e5c3e2c0808cee013
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-10-18 12:40:18 +0530

    Adding centre of mass computation for sparse arrays

--- src/other/com.jl
+++ src/other/com.jl
@@ -19,3 +19,22 @@ function centre_of_mass(raster)
     (xcom_index, ycom_index) =Int.(round.(com ./ sum(skipmissing(raster))))
     return map(getindex, dims(raster), [xcom_index, ycom_index]) # longitude and latitude of the centre of mass
 end
+
+"""
+`centre_of_mass` finds the mean of coordinates weighted by nighttime lights radiance_datacube. 
+
+```julia
+using Rasters
+using SparseArrays
+load_example()
+img = view(radiance_datacube, Ti(1))
+spaster = sparse(Array(img)[:,:,1])
+centre_of_mass(spaster, dims(img))
+```
+"""
+function centre_of_mass(raster, dimensions)
+    nz = findnz(raster)
+    xcom_index = Int(round(sum((nz[1] .* nz[3])) / sum(nz[3])))
+    ycom_index = Int(round(sum((nz[2] .* nz[3])) / sum(nz[3])))
+    return map(getindex, dimensions, [xcom_index, ycom_index]) # longitude and latitude of the centre of mass
+end
