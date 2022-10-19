3476e23937d88c567a7df9c2686e76aea6e6601b
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-10-19 10:29:49 +0530

    adding a function to create a sparse matrix from a raster file

--- src/other/com.jl
+++ src/other/com.jl
@@ -1,25 +1,8 @@
-"""
-`centre_of_mass` finds the mean of coordinates weighted by nighttime lights radiance_datacube. 
-
-```julia
-using Rasters
-load_example()
-img = view(radiance_datacube, Ti(1))
-centre_of_mass(img)
-```
-"""
-function centre_of_mass(raster)
-    com = Tuple(zeros(ndims(raster))) # initializing
-    for i in CartesianIndices(raster)
-        if ismissing(raster[i])
-            continue
-        end
-        com = com .+ Tuple(i) .* raster[i]
-    end
-    (xcom_index, ycom_index) =Int.(round.(com ./ sum(skipmissing(raster))))
-    return map(getindex, dims(raster), [xcom_index, ycom_index]) # longitude and latitude of the centre of mass
+function make_sparse(raster)
+    arr = Array(raster[:,:,1])
+    replace!(arr, missing => 0)
+    sparse(arr)
 end
-
 """
 `centre_of_mass` finds the mean of coordinates weighted by nighttime lights radiance_datacube. 
 
@@ -37,4 +20,19 @@ function centre_of_mass(raster, dimensions)
     xcom_index = Int(round(sum((nz[1] .* nz[3])) / sum(nz[3])))
     ycom_index = Int(round(sum((nz[2] .* nz[3])) / sum(nz[3])))
     return map(getindex, dimensions, [xcom_index, ycom_index]) # longitude and latitude of the centre of mass
+end
+
+"""
+`centre_of_mass` finds the mean of coordinates weighted by nighttime lights radiance_datacube. 
+
+```julia
+using Rasters
+load_example()
+img = view(radiance_datacube, Ti(1))
+centre_of_mass(img)
+```
+"""
+function centre_of_mass(raster)
+    spaster = make_sparse(raster)
+    centre_of_mass(spaster, dims(raster))
 end
