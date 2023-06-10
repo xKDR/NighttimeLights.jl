ffa97a23012cb21405d66631ffc549ea13378c91
ayushpatnaikgit <ayushpatnaik@gmail.com>
2023-06-10 11:46:59 +1000

    The example file had changed slightly. Changing test case accordingly.

--- test/other/com.jl
+++ test/other/com.jl
@@ -1,9 +1,8 @@
-using SparseArrays 
-img = view(radiance_datacube, Ti(1))
+img = view(radiance_datacube, Ti(At(Date("2012-04"))))
 cog = centre_of_mass(img)
 
-@test cog ≈ [72.9125001033, 19.120832886299997]
+@test cog ≈ [72.88333535640002, 19.070832885899996]
 
 img = view(radiance_datacube, Ti(1))
 spaster = sparse(Array(img)[:,:,1])
-centre_of_mass(spaster, dims(img)) ≈ [72.9125001033, 19.120832886299997]
+centre_of_mass(spaster, dims(img)) ≈ [72.88333535640002, 19.070832885899996]
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 using Dates
 using DataFrames
 using Rasters
+using SparseArrays 
 
 fatalerrors = length(ARGS) > 0 && ARGS[1] == "-f"
 quiet = length(ARGS) > 0 && ARGS[1] == "-q"
